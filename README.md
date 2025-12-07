cdd2c63ea34010e4e1764ac60d8f27625b352759
Robert Uhl <rcuhljr@gmail.com>
2025-12-07 11:28:51 -0500

    Day 1 DSL finished

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
 # AoC 2025 - A Domain Specific Language
 
 ## Usage
+Write a problem statement in a `.a25` file, then pass it to the solver to process.
+
+```
+  ruby .\solve.rb day1.a25
+```
+
+### General Language Constructs
+Unless otherwise specified, all keywords must be at the start of line and match exactly.
+
+* `DEBUG ON`/`DEBUG OFF` - Enable/Disable engine debugging at the current script point
+* `PRINT "A message with $INTERPOLATION"` - Display a given message, specified variables can be inserted as desired
+* `#Comments` - Any line starting with a # is ignored
+
+### Day 1 DSL Specification
+
+* `RESET_DIAL` - turn dial to 50, reset password counting
+* `SPIN >>` - Begin a block of spin instructions on the following line
+* `L#`/`R#` - Rotate the dial left or right a given number (#) of clicks
+* `<< SPUN` - Stop reading spin instructions
+* `$PASSWORD` - Variable of the current password value
--- solve.rb
+++ solve.rb
@@ -2,28 +2,57 @@
 
 targetFile = ARGV[0]
 
+debug_flag = false
+
 dayState = [
   { dial_pointer: 50,
     password: 0,
-    spinning: false }
+    spinning: false,
+    alternate_mode: false }
 ]
 
 File.readlines(targetFile).each do |scriptLine|
   case scriptLine.chomp
+  when /^#/
+    next
   # DAY 1
   when /^RESET_DIAL/
     dayState[0][:dial_pointer] = 50
+    dayState[0][:password] = 0
   when /^PRINT "(.*)"/
     message = Regexp.last_match().captures[0]
     puts message.gsub(/\$PASSWORD/, dayState[0][:password].to_s)
   when /^SPIN >>$/
+    debug_msg = ""
     dayState[0][:spinning] = true
   when /^<< SPUN$/
     dayState[0][:spinning] = false
+  when /^DEBUG ON$/
+    debug_flag = true
+  when /^DEBUG OFF$/
+    debug_flag = false
+  when /^PASSWORD_MODE ALTERNATE$/
+    dayState[0][:alternate_mode] = true
+  when /^PASSWORD_MODE PRIMARY$/
+    dayState[0][:alternate_mode] = false
   when /^(L|R)(\d+)$/
+    next unless dayState[0][:spinning]
     vector = Regexp.last_match().captures[0] == "R" ? 1 : -1
-    magn
-
+    magnitude = Regexp.last_match().captures[1].to_i
+    old_target = dayState[0][:dial_pointer]
+    pointer_target = (dayState[0][:dial_pointer] + vector * magnitude)
+    dayState[0][:dial_pointer] = pointer_target % 100
+    if dayState[0][:alternate_mode]
+      if vector < 0
+        pointer_target += 100 if pointer_target < 0 && old_target == 0
+      else
+        pointer_target -= 100 if pointer_target > 0 && pointer_target % 100 == 0
+      end
+      puts "Went past 0 #{(pointer_target/100).abs} times" if debug_flag && (pointer_target/100).abs > 0
+      dayState[0][:password] += (pointer_target/100).abs
+    end
+    dayState[0][:password] += 1 if dayState[0][:dial_pointer] == 0
+    puts "(#{dayState[0][:password]})Rotated #{scriptLine.chomp} from #{old_target} to #{dayState[0][:dial_pointer]}" if debug_flag
   else
   end
 end
